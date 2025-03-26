c5f001885737192d06165d116bd67f0c77a091ac
Julian Andersen <julian.cesar.andersen@gmail.com>
2025-03-26 23:14:38 +0100

    Fix prettyprint function

--- binarySearchTrees.js
+++ binarySearchTrees.js
@@ -11,7 +11,7 @@ class Tree {
         this.array = array
             .sort((a, b) => a - b)
             .filter((item, index, self) => self.indexOf(item) === index);
-        this.root = this.buildTree(this.array, 0, this.array.length - 1);
+        this.root = this._buildTree(this.array, 0, this.array.length - 1);
     }
 
     prettyPrint(node, prefix = "", isLeft = true) {
@@ -21,26 +21,27 @@ class Tree {
         if (node.right !== null) {
             this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
         }
-        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.root}`);
         if (node.left !== null) {
             this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
         }
     };
 
-    buildTree(array, start, end) {
+    _buildTree(array, start, end) {
         if (start > end) return null;
 
         let mid = Math.floor((start + end) / 2);
 
         let root = new Node(array[mid]);
 
-        root.left = this.buildTree(array, start, mid - 1);
-        root.right = this.buildTree(array, mid + 1, end);
+        root.left = this._buildTree(array, start, mid - 1);
+        root.right = this._buildTree(array, mid + 1, end);
 
         return root;
     }
+
+
 }
 
 let tree = new Tree([1, 3, 2, 4]);
-console.log(tree);
 tree.prettyPrint(tree.root);
